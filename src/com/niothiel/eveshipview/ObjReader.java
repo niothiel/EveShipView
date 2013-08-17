ebb78ddd02d3488632759f8b639f29b89c2165a5
Niothiel <niothiel@gmail.com>
2013-08-17 16:54:39 -0400

    Refactored out some code into a dedicated reader instance.

---
+++ src/com/niothiel/eveshipview/ObjReader.java
@@ -0,0 +1,97 @@
+package com.niothiel.eveshipview;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.util.Log;
+
+public class ObjReader {
+	public ArrayList<float[]> vertices = new ArrayList<float[]>();
+	public ArrayList<float[]> textures = new ArrayList<float[]>();
+	public ArrayList<float[]> normals = new ArrayList<float[]>();
+	
+	public ArrayList<int[]> vertexIndicies = new ArrayList<int[]>();
+	public ArrayList<int[]> textureIndicies = new ArrayList<int[]>();
+	public ArrayList<int[]> normalIndicies = new ArrayList<int[]>();
+	
+	public int floatsPerVertex;
+	
+	public ObjReader(Context context, String fileName) {
+		try {
+			InputStream is = context.getResources().getAssets().open(fileName);
+	    	BufferedReader r = new BufferedReader(new InputStreamReader(is));
+	    	
+	    	for(String line = r.readLine(); line != null; line = r.readLine()) {
+	    		if(line.length() == 0 || line.charAt(0) == '#')
+	    			continue;
+	    		
+	    		String[] fields = line.split(" ");
+	    		String command = fields[0];
+	    		
+	    		if(command.equals("v")) {
+	    			float[] vertex = parseFloats(fields);
+	    			floatsPerVertex = vertex.length;
+	    			vertices.add(vertex);
+	    		}
+	    		else if(command.equals("vt"))
+	    			textures.add(parseFloats(fields));
+	    		else if(command.equals("vn"))
+	    			normals.add(parseFloats(fields));
+	    		else if(command.equals("f"))
+	    			parseFace(fields);
+	    	}
+    	}
+    	catch(Exception e) {
+    		Log.e("asdf", "Can't open the requested file!");
+    		return;
+    	}
+	}
+	
+	public FloatBuffer getVerticesBuffer() {
+		// Create the float buffer, The number of floats that we have is: numVertexIndicies * 3 vertexes per index * n floats per vertex
+		FloatBuffer buffer = GLHelper.createFloatBuffer(vertexIndicies.size() * 3 * floatsPerVertex);
+		
+		for(int[] indexes : vertexIndicies) {
+			for(int index : indexes) {
+				buffer.put(vertices.get(index - 1));
+			}
+		}
+		
+		buffer.position(0);
+		return buffer;
+	}
+	
+	private void parseFace(String[] fields) {
+    	int[] vs = new int[fields.length - 1];
+    	int[] vts = new int[fields.length - 1];
+    	int[] vns = new int[fields.length - 1];
+    	
+    	for(int x = 1; x < fields.length; x++) {
+    		String[] field = fields[x].split("/");
+    		int v = Integer.parseInt(field[0]);
+    		int vt = Integer.parseInt(field[1]);
+    		int vn = Integer.parseInt(field[2]);
+    		
+    		vs[x - 1] = v;
+    		vts[x - 1] = vt;
+    		vns[x - 1] = vn;
+    		
+    	}
+    	vertexIndicies.add(vs);
+    	textureIndicies.add(vts);
+    	normalIndicies.add(vns);
+    }
+	
+	private float[] parseFloats(String[] fields) {
+		float[] result = new float[fields.length - 1];
+		
+		for(int x = 1; x < fields.length; x++)
+			result[x - 1] = Float.parseFloat(fields[x]);
+		
+		return result;
+	}
+}
--- src/com/niothiel/eveshipview/ObjRender.java
+++ src/com/niothiel/eveshipview/ObjRender.java
@@ -1,8 +1,5 @@
 package com.niothiel.eveshipview;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -29,8 +26,7 @@ class ObjRender {
 	
 
     private FloatBuffer vertexBuffer;
-    private ArrayList<float[]> vertices;
-    private ArrayList<int[]> faces;
+    private ObjReader objReader;
     
     private int mProgram;
     private int mPositionHandle;
@@ -39,68 +35,14 @@ class ObjRender {
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
-    static float triangleCoords[] = { // in counterclockwise order:
-         0.0f,  0.622008459f, 0.0f,   // top
-        -0.5f, -0.311004243f, 0.0f,   // bottom left
-         0.5f, -0.311004243f, 0.0f    // bottom right
-    };
 
     // Set color with red, green, blue and alpha (opacity) values
     float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
 
     public ObjRender(Context context, String fileName) {
-    	vertices = new ArrayList<float[]>();
-    	faces = new ArrayList<int[]>();
-    	
-    	try {
-	    	final InputStream is = context.getResources().getAssets().open(fileName);
-	    	BufferedReader r = new BufferedReader(new InputStreamReader(is));
-	    	
-	    	for(String line = r.readLine(); line != null; line = r.readLine()) {
-	    		if(line.length() == 0 || line.charAt(0) == '#')
-	    			continue;
-	    		
-	    		String[] fields = line.split(" ");
-	    		String command = fields[0];
-	    		
-	    		if(command.equals("v")) {
-	    			float[] vertex = {
-	    					Float.parseFloat(fields[1]),
-	    					Float.parseFloat(fields[2]),
-	    					Float.parseFloat(fields[3])
-	    			};
-	    			vertices.add(vertex);
-	    		}
-	    		else if(command.equals("f")) {
-	    			parseFace(fields);
-	    		}
-	    	}
-    	}
-    	catch(Exception e) {
-    		Log.e("asdf", "Can't open the requested file!");
-    		return;
-    	}
-    	
-    	Log.d("test", "Number of faces: " + faces.size());
-    	
-        // initialize vertex byte buffer for shape coordinates
-        ByteBuffer bb = ByteBuffer.allocateDirect(
-                // (number of faces * 3 indexes per face * 3 vertices per index * 4 bytes per float)
-                faces.size() * 3 * 3 * 4);
-        // use the device hardware's native byte order
-        bb.order(ByteOrder.nativeOrder());
-
-        // create a floating point buffer from the ByteBuffer
-        vertexBuffer = bb.asFloatBuffer();
-        // add the coordinates to the FloatBuffer
-        for(int[] indexes : faces) {
-        	for(int index : indexes) {
-        		vertexBuffer.put(vertices.get(index - 1));
-        	}
-        }
-        // set the buffer to read the first coordinate
-        vertexBuffer.position(0);
-        
+    	objReader = new ObjReader(context, fileName);
+    	vertexBuffer = objReader.getVerticesBuffer();
+    	        
         // Load up the the vertex and fragment shaders.
         mProgram = GLHelper.loadProgram(vertexShaderCode, fragmentShaderCode);
     }
@@ -116,7 +58,7 @@ class ObjRender {
         GLES20.glEnableVertexAttribArray(mPositionHandle);
 
         // Prepare the triangle coordinate data
-        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+        GLES20.glVertexAttribPointer(mPositionHandle, objReader.floatsPerVertex,
                                      GLES20.GL_FLOAT, false,
                                      0, vertexBuffer);
 
@@ -133,23 +75,9 @@ class ObjRender {
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
         
         // Draw the triangle
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, faces.size() * 3);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, objReader.vertexIndicies.size() * 3);
 
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(mPositionHandle);
-    }
-    
-    private void parseFace(String[] fields) {
-    	int[] vs = new int[fields.length - 1];
-    	
-    	for(int x = 1; x < fields.length; x++) {
-    		String[] field = fields[x].split("/");
-    		int v = Integer.parseInt(field[0]);
-    		int vt = Integer.parseInt(field[1]);
-    		int vn = Integer.parseInt(field[2]);
-    		
-    		vs[x - 1] = v;
-    	}
-    	faces.add(vs);
     }
 }
